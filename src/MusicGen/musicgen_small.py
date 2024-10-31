f216249956b4ec92e845b782c02a1458b80777cf
Mateusz Tobiasz <mati11.2001@gmail.com>
2024-10-31 23:52:30 +0100

    Added poetry to manage dependecies and build the project

--- src/MusicGen/musicgen_small.py
+++ src/MusicGen/musicgen_small.py
@@ -1,27 +1,33 @@
-from peft import PeftConfig, PeftModel
-from transformers import AutoProcessor, AutoModelForTextToWaveform, MusicgenForConditionalGeneration
-
 import os
-import torch
-import soundfile as sf
 
+import soundfile as sf
+import torch
+from peft import PeftConfig, PeftModel
+from transformers import (
+    AutoModelForTextToWaveform,
+    AutoProcessor,
+    MusicgenForConditionalGeneration,
+)
 
 
 def generate():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     processor = AutoProcessor.from_pretrained("facebook/musicgen-small")
-    model = MusicgenForConditionalGeneration.from_pretrained("facebook/musicgen-small").to(device)
+    model = MusicgenForConditionalGeneration.from_pretrained(
+        "facebook/musicgen-small"
+    ).to(device)
 
     inputs = processor(
         text=["80s punk and pop track with bassy drums and synth"],
         padding=True,
-        return_tensors="pt"
+        return_tensors="pt",
     ).to(device)
 
-
-    inputs = processor(text=["80s punk and pop track with bassy drums and synth"],
+    inputs = processor(
+        text=["80s punk and pop track with bassy drums and synth"],
         padding=True,
-        return_tensors="pt").to(device)
+        return_tensors="pt",
+    ).to(device)
 
     audio_values = model.generate(**inputs, max_new_tokens=256)
 
@@ -29,28 +35,31 @@ def generate():
     audio_values = audio_values.cpu().float().numpy()
     sf.write("punk_orig.wav", audio_values[0].T, sampling_rate)
 
+
 def load():
     device = torch.device("cuda:0" if torch.cuda.is_available() else "cpu")
     checkpoint_path = f"{os.getcwd()}/musicgen-dreamboothing/musicgen-small-lora-punk/"  # Replace with the actual path to your checkpoint directory
 
     config = PeftConfig.from_pretrained(checkpoint_path)
     base_model = AutoModelForTextToWaveform.from_pretrained(
-        config.base_model_name_or_path,
-        torch_dtype=torch.float16
+        config.base_model_name_or_path, torch_dtype=torch.float16
     )
 
     model = PeftModel.from_pretrained(base_model, checkpoint_path).to(device)
     processor = AutoProcessor.from_pretrained(checkpoint_path)
 
-    inputs = processor(text=["80s punk and pop track with bassy drums and synth"],
+    inputs = processor(
+        text=["80s punk and pop track with bassy drums and synth"],
         padding=True,
-        return_tensors="pt").to(device)
+        return_tensors="pt",
+    ).to(device)
 
     audio_values = model.generate(**inputs, max_new_tokens=256)
 
     sampling_rate = model.config.audio_encoder.sampling_rate
     audio_values = audio_values.cpu().float().numpy()
     sf.write("punk_fined.wav", audio_values[0].T, sampling_rate)
+
 
 if __name__ == "__main__":
     generate()
