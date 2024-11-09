4f96a5ffc039ceacee2a78d6dba56360fe373b8d
Mikołaj Bańkowski <108618874+mik00laj@users.noreply.github.com>
2024-11-09 21:33:18 +0100

    Update watermark_generator.py

--- wimudp/watermarking/audio/generation/watermark_generator.py
+++ wimudp/watermarking/audio/generation/watermark_generator.py
@@ -5,6 +5,10 @@ import torchaudio
 def generate_watermark(audio_path, alpha=1, output_path="watermarked_audio.wav"):
     model = AudioSeal.load_generator("audioseal_wm_16bits")
     audio, sample_rate = torchaudio.load(audio_path)
-    watermarked_audio = model(audio.unsqueeze(0), sample_rate=sample_rate, alpha=alpha)
-    torchaudio.save(output_path, watermarked_audio.detach().squeeze(0), sample_rate)
+    audio = audio.unsqueeze(0) 
+    watermark = model.get_watermark(audio, sample_rate=sample_rate)
+    watermarked_audio = audio + watermark * alpha
+    watermarked_audio = watermarked_audio.squeeze(0)
+    torchaudio.save(output_path, watermarked_audio.detach(), sample_rate)
+    
     return watermarked_audio
