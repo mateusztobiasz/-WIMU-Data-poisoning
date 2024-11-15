ec4a697bced527d9e57e10e0901f4904d58a4b4c
mtobiasz <mati11.2001@gmail.com>
2024-11-15 01:17:02 +0100

    Downgraded python to 3.11 in order to install audioldm

--- wimudp/watermarking/audio/utils/audio_utils.py
+++ wimudp/watermarking/audio/utils/audio_utils.py
@@ -3,6 +3,7 @@
 import os
 
 import librosa
+import librosa.display as ld
 import matplotlib.pyplot as plt
 import numpy as np
 import soundfile as sf
@@ -66,7 +67,7 @@ class AudioUtils:
         plt.figure(figsize=(20, 25))
 
         plt.subplot(3, 1, 1)
-        librosa.display.waveshow(y, sr=sr)
+        ld.waveshow(y, sr=sr)
         plt.title("Waveform")
         plt.xlabel("Time (s)")
         plt.ylabel("Amplitude")
@@ -78,7 +79,7 @@ class AudioUtils:
         plt.ylabel("Magnitude")
 
         plt.subplot(3, 1, 3)
-        librosa.display.specshow(stft, sr=sr, x_axis="time", y_axis="hz", cmap="magma")
+        ld.specshow(stft, sr=sr, x_axis="time", y_axis="hz", cmap="magma")
         plt.colorbar(format="%+2.0f dB")
         plt.title("Spectrogram (STFT)")
         plt.xlabel("Time (s)")
