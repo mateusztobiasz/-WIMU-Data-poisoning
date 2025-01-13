4a878c51b435669c3e0bbdeb6e6b2701bdf1fad5
Maciejoss <maciejkordyaczny@gmail.com>
2025-01-13 19:34:53 +0100

    add stages display

--- wimudp/data_poisoning/generate_nightshade.py
+++ wimudp/data_poisoning/generate_nightshade.py
@@ -15,16 +15,26 @@ def main():
 
     samples_number = int(samples_number) if samples_number.isdigit() else None
 
+    display_stage("Filtering data")
     df_filtered = process_csv_file(concept_a=concept_a or None, concept_c_action=concept_c_action or None, rows_number=rows_number)
     df_filtered.head(rows_number).to_csv("./data/audiocaps_{concept_c}.csv")
     print("Filtered DataFrame:")
     print(df_filtered)
 
+    display_stage("Extracting samples")
     df_samples = get_samples(concept_c=concept_c or None, concept_c_action=concept_c_action_extractor or None, samples_number=samples_number)
     print("Extracted Samples:")
     print(df_samples)
 
+    display_stage("Generating poisoned data")
     generate_all(df_samples)
+
+def display_stage(stage_name):
+    print("\n" + "=" * 50)
+    print(f"{' ' * ((50 - len(stage_name)) // 2)}{stage_name.upper()}")
+    print("=" * 50 + "\n")
+
+display_stage("Filtering Data")
 
 if __name__ == "__main__":
     main()
